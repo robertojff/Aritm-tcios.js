f3c8e355fe35ca610ce160c93561624bd81c584a
robertojff <robertojff3333@gmail.com>
2023-12-21 20:11:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let idadeDaCriança = 18;
+
+console.log(--idadeDaCriança);
+
+let idadeDoAdulto = 35;
+
+console.log(++idadeDoAdulto);
+
+let idadeDoIdoso = 61;
+
+console.log(+-idadeDoIdoso);
+
+let salario = 1.280;
+
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(salario ** salario);
+
+let contaDeAgua = 60;
+
+console.log(contaDeAgua + contaDeAgua);
+console.log(contaDeAgua - contaDeAgua);
+console.log(contaDeAgua * contaDeAgua);
+console.log(contaDeAgua / contaDeAgua);
+console.log(contaDeAgua ** contaDeAgua);
+
